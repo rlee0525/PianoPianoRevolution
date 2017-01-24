bc477f8601b875ff3362efb96bf6a014f48174a0
Raymond Lee <raylee0525@gmail.com>
2017-01-23 21:37:53 -0800

    Add jQuery for start button and configured canvas

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 module.exports = {
   entry: "./lib/ppr.js",
   output: {
-  	filename: "./lib/bundle.js"
+   filename: "./lib/bundle.js"
   },
-
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
   devtool: 'source-map',
 };
